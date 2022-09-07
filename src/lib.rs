13fc48b7bab9b046c8c726883d0e814e1e943cd7
Thomas BESSOU <thomas.bessou@hotmail.fr>
2022-09-07 02:15:46 +0200

    simplify thousands_separate

--- src/lib.rs
+++ src/lib.rs
@@ -3,9 +3,7 @@
 //! It is still under development and the documentation isn't good. However, the following example may be helpful:
 //!
 //! ```
-//! use mcts::*;
-//! use mcts::tree_policy::*;
-//! use mcts::transposition_table::*;
+//! use mcts::{transposition_table::*, tree_policy::*, *};
 //!
 //! // A really simple game. There's one player and one number. In each move the player can
 //! // increase or decrease the number. The player's score is the number.
@@ -18,89 +16,105 @@
 //!
 //! #[derive(Clone, Debug, PartialEq)]
 //! enum Move {
-//!     Add, Sub
+//! 	Add,
+//! 	Sub,
 //! }
 //!
 //! impl GameState for CountingGame {
-//!     type Move = Move;
-//!     type Player = ();
-//!     type MoveList = Vec<Move>;
+//! 	type Move = Move;
+//! 	type Player = ();
+//! 	type MoveList = Vec<Move>;
 //!
-//!     fn current_player(&self) -> Self::Player {
-//!         ()
-//!     }
-//!     fn available_moves(&self) -> Vec<Move> {
-//!         let x = self.0;
-//!         if x == 100 {
-//!             vec![]
-//!         } else {
-//!             vec![Move::Add, Move::Sub]
-//!         }
-//!     }
-//!     fn make_move(&mut self, mov: &Self::Move) {
-//!         match *mov {
-//!             Move::Add => self.0 += 1,
-//!             Move::Sub => self.0 -= 1,
-//!         }
-//!     }
+//! 	fn current_player(&self) -> Self::Player {
+//! 		()
+//! 	}
+//! 	fn available_moves(&self) -> Vec<Move> {
+//! 		let x = self.0;
+//! 		if x == 100 {
+//! 			vec![]
+//! 		} else {
+//! 			vec![Move::Add, Move::Sub]
+//! 		}
+//! 	}
+//! 	fn make_move(&mut self, mov: &Self::Move) {
+//! 		match *mov {
+//! 			Move::Add => self.0 += 1,
+//! 			Move::Sub => self.0 -= 1,
+//! 		}
+//! 	}
 //! }
 //!
 //! impl TranspositionHash for CountingGame {
-//!     fn hash(&self) -> u64 {
-//!         self.0 as u64
-//!     }
+//! 	fn hash(&self) -> u64 {
+//! 		self.0 as u64
+//! 	}
 //! }
 //!
 //! struct MyEvaluator;
 //!
 //! impl Evaluator<MyMCTS> for MyEvaluator {
-//!     type StateEvaluation = i64;
+//! 	type StateEvaluation = i64;
 //!
-//!     fn evaluate_new_state(&self, state: &CountingGame, moves: &Vec<Move>,
-//!         _: Option<SearchHandle<MyMCTS>>)
-//!         -> (Vec<()>, i64) {
-//!         (vec![(); moves.len()], state.0)
-//!     }
-//!     fn interpret_evaluation_for_player(&self, evaln: &i64, _player: &()) -> i64 {
-//!         *evaln
-//!     }
-//!     fn evaluate_existing_state(&self, _: &CountingGame,  evaln: &i64, _: SearchHandle<MyMCTS>) -> i64 {
-//!         *evaln
-//!     }
+//! 	fn evaluate_new_state(
+//! 		&self,
+//! 		state: &CountingGame,
+//! 		moves: &Vec<Move>,
+//! 		_: Option<SearchHandle<MyMCTS>>,
+//! 	) -> (Vec<()>, i64) {
+//! 		(vec![(); moves.len()], state.0)
+//! 	}
+//! 	fn interpret_evaluation_for_player(&self, evaln: &i64, _player: &()) -> i64 {
+//! 		*evaln
+//! 	}
+//! 	fn evaluate_existing_state(
+//! 		&self,
+//! 		_: &CountingGame,
+//! 		evaln: &i64,
+//! 		_: SearchHandle<MyMCTS>,
+//! 	) -> i64 {
+//! 		*evaln
+//! 	}
 //! }
 //!
 //! #[derive(Default)]
 //! struct MyMCTS;
 //!
 //! impl MCTS for MyMCTS {
-//!     type State = CountingGame;
-//!     type Eval = MyEvaluator;
-//!     type NodeData = ();
-//!     type ExtraThreadData = ();
-//!     type TreePolicy = UCTPolicy;
-//!     type TranspositionTable = ApproxTable<Self>;
+//! 	type State = CountingGame;
+//! 	type Eval = MyEvaluator;
+//! 	type NodeData = ();
+//! 	type ExtraThreadData = ();
+//! 	type TreePolicy = UCTPolicy;
+//! 	type TranspositionTable = ApproxTable<Self>;
 //!
-//!     fn cycle_behaviour(&self) -> CycleBehaviour<Self> {
-//!         CycleBehaviour::UseCurrentEvalWhenCycleDetected
-//!     }
+//! 	fn cycle_behaviour(&self) -> CycleBehaviour<Self> {
+//! 		CycleBehaviour::UseCurrentEvalWhenCycleDetected
+//! 	}
 //! }
 //!
 //! let game = CountingGame(0);
-//! let mut mcts = MCTSManager::new(game, MyMCTS, MyEvaluator, UCTPolicy::new(0.5),
-//!     ApproxTable::new(1024));
+//! let mut mcts = MCTSManager::new(
+//! 	game,
+//! 	MyMCTS,
+//! 	MyEvaluator,
+//! 	UCTPolicy::new(0.5),
+//! 	ApproxTable::new(1024),
+//! );
 //! mcts.playout_n_parallel(10000, 4); // 10000 playouts, 4 search threads
 //! mcts.tree().debug_moves();
 //! assert_eq!(mcts.best_move().unwrap(), Move::Add);
-//! assert_eq!(mcts.principal_variation(50),
-//!     vec![Move::Add; 50]);
-//! assert_eq!(mcts.principal_variation_states(5),
-//!     vec![
-//!         CountingGame(0),
-//!         CountingGame(1),
-//!         CountingGame(2),
-//!         CountingGame(3),
-//!         CountingGame(4),
-//!         CountingGame(5)]);
+//! assert_eq!(mcts.principal_variation(50), vec![Move::Add; 50]);
+//! assert_eq!(
+//! 	mcts.principal_variation_states(5),
+//! 	vec![
+//! 		CountingGame(0),
+//! 		CountingGame(1),
+//! 		CountingGame(2),
+//! 		CountingGame(3),
+//! 		CountingGame(4),
+//! 		CountingGame(5)
+//! 	]
+//! );
 //! ```
 
 extern crate vec_storage_reuse;
@@ -112,14 +126,13 @@ pub mod transposition_table;
 pub mod tree_policy;
 
 pub use search_tree::*;
-use transposition_table::*;
-use tree_policy::*;
+use {transposition_table::*, tree_policy::*};
 
-use atomics::*;
-use std::sync::Arc;
-use std::thread::JoinHandle;
-use std::time::Duration;
-use vec_storage_reuse::VecStorageForReuse;
+use {
+	atomics::*,
+	std::{sync::Arc, thread::JoinHandle, time::Duration},
+	vec_storage_reuse::VecStorageForReuse,
+};
 
 pub trait MCTS: Sized + Sync {
 	type State: GameState + Sync;
@@ -417,13 +430,13 @@ where
 // https://stackoverflow.com/questions/26998485/rust-print-format-number-with-thousand-separator
 fn thousands_separate(x: usize) -> String {
 	let s = format!("{}", x);
-	let bytes: Vec<_> = s.bytes().rev().collect();
-	let chunks: Vec<_> = bytes
-		.chunks(3)
-		.map(|chunk| String::from_utf8(chunk.to_vec()).unwrap())
+	let chunks: Vec<&str> = s
+		.as_bytes()
+		.rchunks(3)
+		.rev()
+		.map(|chunk| std::str::from_utf8(chunk).unwrap())
 		.collect();
-	let result: Vec<_> = chunks.join(",").bytes().rev().collect();
-	String::from_utf8(result).unwrap()
+	chunks.join(",")
 }
 
 #[must_use]
